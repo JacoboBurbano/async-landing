3e853bbaa159981f744ceb2cf25a9a4a6f1032e1
Jacobo <jacobo@Leos-MacBook-Air.local>
2023-06-27 08:45:31 -0500

    Cambios al consumir el api e insercion de nuva informacion

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,12 +1,13 @@
-const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCw05fUBPwmpu-ehXFMqfdMw&part=snippet%2Cid&order=date&maxResults=9';
+const API = 'https://youtube-v3-alternative.p.rapidapi.com/channel?id=UCr-C1JFhARK4AMKOFTd40Hg';
 
-const content = null || document.getElementById('content');
-
+const content = document.getElementById('content');
+const imageChannel = document.getElementById('image-channel');
+const infoChannel = document.getElementById('info-channel');
 const options = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
-    'X-RapidAPI-Key': 'c68edbdba9mshc3cb6eb8038f3b2p1f444ajsna5de19fe1256'
+    'X-RapidAPI-Host': 'youtube-v3-alternative.p.rapidapi.com',
+    'X-RapidAPI-Key': '492860d8femsh4a3c0866517ec05p1559a6jsnd70bf685a280'
   }
 };
 
@@ -19,23 +20,29 @@ async function fetchData(urlApi) {
 (async () => {
   try {
     const videos = await fetchData(API);
-    let view = `
-    ${videos.items.map(video => `
+    imageChannel.src =  videos.meta.thumbnail[0].url
+    let viewChannel = `<h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+    <span class="block xl:inline">${videos.meta.title}</span>
+    <span class="block text-indigo-600 xl:inline">@improther6943</span>
+    </h1>`
+    let viewVideos = `
+    ${videos.data.map(video => `
       <div class="group relative">
         <div
           class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-          <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+          <img src="${video.thumbnail[0].url}" alt="${video.description}" class="w-full">
         </div>
         <div class="mt-4 flex justify-between">
           <h3 class="text-sm text-gray-700">
             <span aria-hidden="true" class="absolute inset-0"></span>
-            ${video.snippet.title}
+            ${video.title}
           </h3>
         </div>
       </div>
     `).slice(0, 4).join('')}
     `;
-    content.innerHTML = view;
+    content.innerHTML = viewVideos;
+    infoChannel.innerHTML = viewChannel;
   } catch (error) {
     console.log(error);
   }
